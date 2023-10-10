6692e260b6016fe5c1138022351892f9f473fc03
ShaneKavinda <shanekavinda1999@gmail.com>
2023-10-11 00:49:50 +0530

    added react-hot-toast

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import toast from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <></>
+    <div>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
+    </div>
   )
 }
